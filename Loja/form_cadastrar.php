8f9b4cd601598ae4d3c5db5ef375628b454572ee
PietroVersollato <161628491+PietroVersollato@users.noreply.github.com>
2025-08-15 17:13:55 -0300

    nlanalalnallaa

---
+++ Loja/form_cadastrar.php
@@ -0,0 +1,26 @@
+<?php
+include 'cabecalho.php';
+?>
+
+<body>
+    <div class="container">
+        <form>
+            <div class="mb-3">
+                <input class="form-control" type="texte" name="produto" placeholder="Digite o nome do Produto">
+            </div>
+            <div class="mb-3">
+                <input class="form-control" type="texte" name="produto" placeholder="Digite o preço do Produto">
+            </div>
+            <div class="mb-3">
+                <input class="form-control" type="texte" name="produto" placeholder="Digite a quantidade">
+            </div>
+            <button type="submit" class="btn btn-primary">Cadastrar</button>
+    </div>
+    </form>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous">
+        </script>
+</body>
+
+</html>
