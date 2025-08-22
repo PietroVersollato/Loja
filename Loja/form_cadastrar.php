ba55b38554cdd525ea7d32371919c67eee65cd72
PietroVersollato <161628491+PietroVersollato@users.noreply.github.com>
2025-08-22 18:22:32 -0300

    Aula de Hoje

--- Loja/form_cadastrar.php
+++ Loja/form_cadastrar.php
@@ -4,15 +4,16 @@ include 'cabecalho.php';
 
 <body>
     <div class="container">
-        <form>
+        <h2>CADASTRO DE PRODUTO</h2>
+        <form action="inserir.php" method="POST">
             <div class="mb-3">
                 <input class="form-control" type="texte" name="produto" placeholder="Digite o nome do Produto">
             </div>
             <div class="mb-3">
-                <input class="form-control" type="texte" name="produto" placeholder="Digite o preço do Produto">
+                <input class="form-control" type="texte" name="preco" placeholder="Digite o preço do Produto">
             </div>
             <div class="mb-3">
-                <input class="form-control" type="texte" name="produto" placeholder="Digite a quantidade">
+                <input class="form-control" type="texte" name="quantidade" placeholder="Digite a quantidade">
             </div>
             <button type="submit" class="btn btn-primary">Cadastrar</button>
     </div>
--- Loja/listar.php
+++ Loja/listar.php
@@ -17,18 +17,27 @@ include 'cabecalho.php';
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>Biriri</td>
-                    <td>R$ 3000.99</td>
-                    <td>10</td>
+
+                <?php
+                require 'conexao.php';
+                $sql = "SELECT * FROM produtos";
+                $stmt = $pdo->query($sql);
+                while ($produto = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                    echo "<tr>";
+                    echo"<td>". $produto['id'] ."</td>";
+                    echo"<td>". $produto['nome'] ."</td>";
+                    echo"<td>". $produto['preco'] ."</td>";
+                    echo"<td>". $produto['quantidade'] ."</td>";
+                    echo"
                     <td>
-                        <div class="btn-group" role="group" aria-label="Basic mixed styles example">
-                            <a href="#" type="button" class="btn btn-danger">Atualizar</a>
-                            <a href="#" type="button" class="btn btn-warning">Apagar</a>
+                        <div class=btn-group' role='group' aria-label='Basic mixed styles example'>
+                            <a href='#' type='button' class='btn btn-danger'>Atualizar</a>
+                            <a href='#' type='button' class='btn btn-warning'>Apagar</a>
                         </div>
-                    </td>
-                </tr>
+                    </td>";
+                    echo "</tr>";
+                  }
+                ?>
             </tbody>
         </table>
     </div>
