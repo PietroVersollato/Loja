5d198b855363a18ab0819b762167efad8d4d5feb
PietroVersollato <pietro.versollato@etec.sp.gov.br>
2025-09-20 23:18:05 -0300

    dexculpa mãe

--- listar.php
+++ listar.php
@@ -11,22 +11,27 @@ $produtos = $pdo->query("SELECT * FROM produtos ORDER BY id DESC")->fetchAll();
   <table class="table table-striped table-hover">
     <thead class="table-primary">
       <tr>
-        <th>ID</th><th>Nome</th><th>Preço (R$)</th><th>Quantidade</th><th>Ações</th>
+        <th>ID</th>
+        <th>Nome</th>
+        <th>Preço (R$)</th>
+        <th>Quantidade</th>
+        <th>Ações</th>
       </tr>
     </thead>
     <tbody>
       <?php foreach ($produtos as $p): ?>
-      <tr>
-        <td><?= $p['id'] ?></td>
-        <td><?= htmlspecialchars($p['nome']) ?></td>
-        <td><?= number_format($p['preco'],2,',','.') ?></td>
-        <td><?= $p['quantidade'] ?></td>
-        <td>
-          <a href="form_atualizar.php?id=<?= $p['id'] ?>" class="btn btn-warning btn-sm"><i class="fa fa-pencil"></i></a>
-          <a href="apagar.php?id=<?= $p['id'] ?>" class="btn btn-danger btn-sm"
-             onclick="return confirm('Excluir este produto?');"><i class="fa fa-trash"></i></a>
-        </td>
-      </tr>
+        <tr>
+          <td><?= $p['id'] ?></td>
+          <td><?= htmlspecialchars($p['nome']) ?></td>
+          <td><?= number_format($p['preco'], 2, ',', '.') ?></td>
+          <td><?= $p['quantidade'] ?></td>
+          <td>
+            <a href="form_atualizar.php?id=<?= $p['id'] ?>" class="btn btn-warning btn-sm"><i
+                class="fa fa-pencil"></i></a>
+            <a href="apagar.php?id=<?= $p['id'] ?>" class="btn btn-danger btn-sm"
+              onclick="return confirm('Excluir este produto?');"><i class="fa fa-trash"></i></a>
+          </td>
+        </tr>
       <?php endforeach; ?>
     </tbody>
   </table>
